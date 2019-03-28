4bbe6a276bbcab7b3895b83f1f060752f9849ee1
Blake Epstein <blake@brightforestmedia.com>
2019-03-28 12:51:43 -0500

    updates

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'activerecord', '~> 5.0', '>= 5.0.0.1'
 gem 'jquery-rails'
 gem 'http', '~> 4.0'
 gem 'bootstrap', '~> 4.1', '>= 4.1.3'
+gem 'meta-tags'
+
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def index
+
+    @page_title       = 'Stillwell Counseling'
+    @page_description = 'Offering individual and family therapy services to Clay Center, KS.'
+    
+
+
     respond_to do |format|
       format.html {}
       format.js {}
